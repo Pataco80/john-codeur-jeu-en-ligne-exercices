a54e9429eb038e0b8d50d14fae3acff13648e948
Pataco80 <pataco80@gmail.com>
2018-02-14 15:50:40 +0100

    Rename scripts.js en exercices.js pour les cours avant la création du jeu su serpent

---
+++ exercices.js
@@ -0,0 +1,36 @@
+// Exercice 4 - Numbers et Strings
+// Variables List
+var name = 'Ricardo';
+var a = 33.89;
+var b = "44";
+var aTostring = a.toString();
+var nameLength = name.length;
+
+var cString = "88";
+var cNumber = parseInt(cString);
+
+var dString = "55.43";
+var dNumber = parseInt(dString);
+
+var eString = "33.47";
+var nuberFloat = parseFloat(eString);
+
+// Console log list
+console.log(name);
+console.log(a);
+console.log(b);
+console.log(aTostring);
+console.log(nameLength);
+console.log(cNumber);
+console.log(dNumber);
+console.log(nuberFloat);
+
+
+
+
+
+// Exercice 4 - Operators
+// Variables List
+
+
+// Console log list
